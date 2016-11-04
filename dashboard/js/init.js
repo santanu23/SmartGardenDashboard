dbaf41738a678935818f9c523a24d385cf375ced
santanu23 <sarkersantanu@gmail.com>
2016-11-04 01:11:35 -0400

    Added HANA db support, line chart

--- dashboard/js/init.js
+++ dashboard/js/init.js
@@ -1,43 +1,121 @@
+var apiUrl = window.location.origin + "/api";
+var theta = 1;
+function randomScalingFactor() {
+	//return Math.round(Math.random() * 100 * (Math.random() > 0.5 ? -1 : 1));
+	return Math.sin(theta + Math.random()*0.5);
+}
+
+function	generatePureSin(){
+	return Math.sin(theta);
+}
+
+function randomColorFactor() {
+	return Math.round(Math.random() * 255);
+}
+
+function randomColor(opacity) {
+	return 'rgba(' + randomColorFactor() + ',' + randomColorFactor() + ',' + randomColorFactor() + ',' + (opacity || '.3') + ')';
+}
+
+function newDate(days) {
+	return moment().add(days, 's').toDate();
+}
+
+function newDateString(days) {
+	return moment().add(days, 's').format();
+}
+
+function generateLabel(labelName) {
+	return labelName.replace('_', ' ');
+}
+
+var config = {
+	type: 'line',
+	data: {
+		labels: [newDate(0), newDate(1), newDate(2), newDate(3), newDate(4), newDate(5), newDate(6)], // Date Objects
+		datasets: [{
+			label: "My First dataset",
+			data: [randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor()],
+			fill: false,
+			borderDash: [5, 5],
+		}, {
+			label: "My Second dataset",
+			data: [randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor()],
+		}, {
+			label: "Dataset with point data",
+			data: [{
+				x: newDateString(0),
+				y: randomScalingFactor()
+			}, {
+				x: newDateString(5),
+				y: randomScalingFactor()
+			}, {
+				x: newDateString(7),
+				y: randomScalingFactor()
+			}, {
+				x: newDateString(15),
+				y: randomScalingFactor()
+			}],
+			fill: false
+		}]
+	},
+	options: {
+		responsive: true,
+	    title:{
+	        display:true,
+	        text:"Chart.js Time Scale"
+	    },
+		scales: {
+			xAxes: [{
+				type: "time",
+				scaleLabel: {
+					display: true,
+					labelString: 'Date'
+				}
+			}, ],
+			yAxes: [{
+				scaleLabel: {
+					display: true,
+					labelString: 'value'
+				}
+			}]
+		},
+	}
+};
+
 (function($){
   $(function(){
-
-    $('.button-collapse').sideNav();
-    var ctx = document.getElementById("myChart");
-	var myChart = new Chart(ctx, {
-	    type: 'bar',
-	    data: {
-	        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-	        datasets: [{
-	            label: '# of Votes',
-	            data: [12, 19, 3, 5, 2, 3],
-	            backgroundColor: [
-	                'rgba(255, 99, 132, 0.2)',
-	                'rgba(54, 162, 235, 0.2)',
-	                'rgba(255, 206, 86, 0.2)',
-	                'rgba(75, 192, 192, 0.2)',
-	                'rgba(153, 102, 255, 0.2)',
-	                'rgba(255, 159, 64, 0.2)'
-	            ],
-	            borderColor: [
-	                'rgba(255,99,132,1)',
-	                'rgba(54, 162, 235, 1)',
-	                'rgba(255, 206, 86, 1)',
-	                'rgba(75, 192, 192, 1)',
-	                'rgba(153, 102, 255, 1)',
-	                'rgba(255, 159, 64, 1)'
-	            ],
-	            borderWidth: 1
-	        }]
-	    },
-	    options: {
-	        scales: {
-	            yAxes: [{
-	                ticks: {
-	                    beginAtZero:true
-	                }
-	            }]
-	        }
-	    }
-	});
+    updateLastReadings();
+    $('.button-collapse').sideNav();   
+    $("#chart-container").text(""); 
+	$("#chart-container").append("<canvas id=\"myChart\"width=\"200\" height=\"100\"></canvas>");
+	var ctx = document.getElementById("myChart").getContext("2d");
+	window.myLine = new Chart(ctx, config);		    
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+queryHana = function(url, response){	
+    $.get(url, function(data, status){
+    	response = data.d.results;
+    });
+}
+
+updateLastReadings = function(){
+	$.get(apiUrl + "/lastReading", function(data, status){
+		var lastReadings = data.d.results[0];
+    	if (lastReadings){
+	    	if(lastReadings.C_LIGHT){
+	    		$('#w_lux').text(parseFloat(lastReadings.C_LIGHT));
+	    	}
+	    	if(lastReadings.C_WATERLEVEL){
+	    		$('#w_water').text(parseFloat(lastReadings.C_WATERLEVEL));
+	    	}
+	    	if(lastReadings.C_MOISTURE){
+	    		$('#w_mos').text(parseFloat(lastReadings.C_MOISTURE));
+	    	}
+	    	if(lastReadings.C_TEMPERATURE){
+	    		$('#w_temp').text(parseFloat(lastReadings.C_TEMPERATURE));
+	    	}
+    	}
+    });	
+}
--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 // call the packages we need
 var express    = require('express');
 var bodyParser = require('body-parser').json();
-var MongoClient = require('mongodb').MongoClient
 var Server = require('mongodb').Server;
 var assert = require('assert');
 var https = require('https');
@@ -13,44 +12,38 @@ var app = express(); // define our app using express
 app.use(bodyParser);
 
 var apiRouter = express.Router(); // get an instance of the express Router
-apiRouter.get('/', function(req, res) {
-    console.log("GET handler");
-	var mongoClient = new MongoClient(new Server('localhost', 27017));
-    mongoClient.open(function(err, mongoClient) {
-        if(err){
-            console.log(err);
-            res.status(500).json({message : 'Error connection to the database'});
-        }else{
-            var db = mongoClient.db("sensorData");
-            var collection = db.collection("collection");
-            // Here we will find all students
-            collection.find({}).toArray(function(err, students) {
-               // so now, we can return all students to the screen.
-               res.status(200).json({'data' : students});
-            });
-        }
-    })
+
+var baseGetUrl = "https://P1942282309:9032822491Pp_@iotmmsp1942282309trial.hanatrial.ondemand.com/com.sap.iotservices.mms/v1/api/http/app.svc/NEO_8IJ3RTA7M5XQSNIBTRTYHEOE5.T_IOT_EA1A61EDC8EA65791306?$format=json" +
+                 "&$select=G_DEVICE,G_CREATED,C_TIMESTAMP,C_LIGHT,C_WATERLEVEL,C_MOISTURE,C_TEMPERATURE,C_DEVICEID";
+
+var httpsCall = function(url, response){
+    https.get(url, (res) => {
+            res.on('data', (data) => {            
+                response.writeHead(200, {
+                    'Content-Type': 'text/json'
+                });
+                response.write(data);
+                response.end();
+          });
+
+        }).on('error', (e) => {
+          console.error(e);
+    });
+}
+
+apiRouter.get('/', function(request, response) {
+    console.log("GET HANA ALL");
+    httpsCall(baseGetUrl, response);  
 });
 
-apiRouter.get('/hana/', function(req, res) {
-    console.log("GET handler for HANA");
-    var url = "https://P1942282309:9032822491Pp_@iotmmsp1942282309trial.hanatrial.ondemand.com/com.sap.iotservices.mms/v1/api/http/app.svc/NEO_8IJ3RTA7M5XQSNIBTRTYHEOE5.T_IOT_EA1A61EDC8EA65791306?$format=json";
-    https.get(url, (res) => {
-      console.log('statusCode:', res.statusCode);
-      console.log('headers:', res.headers);
-
-      res.on('data', (d) => {
-        process.stdout.write(d);
-      });
-
-    }).on('error', (e) => {
-      console.error(e);
-    });
-
+apiRouter.get('/lastReading', function(request, response) {
+    console.log("GET HANA Last Read");
+    var url = baseGetUrl + "&$orderby=G_CREATED desc&$top=1";
+    httpsCall(url,response);
 });
 
 apiRouter.post('/', function(req, res) {
-    console.log("POST handler");
+    console.log("HANA POST");
 	var sensorValue = req.body.sensorValue;
 	var sensorType = req.body.sensorType;
 	var deviceName = req.body.deviceName;
@@ -59,59 +52,7 @@ apiRouter.post('/', function(req, res) {
         return;
     }
 	var timestamp = new Date();
-	var objectToStore = {"sensor":{"type":sensorType,"value":sensorValue},"deviceName": deviceName, "timestamp": timestamp};
-	var mongoClient = new MongoClient(new Server('localhost', 27017));
-    mongoClient.open(function(err, mongoClient) {
-        if(err){
-            console.log(err);
-            res.status(500).json({message : 'Error connection to the database'});
-        }else{
-        	var db = mongoClient.db("sensorData");
-            db.open(function(err, db) {
-			  // Fetch a collection to insert document into
-				var collection = db.collection("collection");
-			  	// Insert a single document
-				collection.insert(objectToStore,{}, function(err, result) {
-    		    	assert.equal(null, err); //make sure error is null
-    		    	// Fetch the document
-    		    	collection.findOne({'_id': result[0]._id}, function(err, item) {
-    		      		assert.equal(null, err); //make sure error is null
-    		      		assert.equal(deviceName, item.deviceName); //make sure the device names match
-    		      		db.close();
-                        res.status(200).json({message : 'Successfully Inserted', dataEntry : result});
-    		    	})
-			  	});
-			});
-        }
-    })
-});
-//TODO: change all methods to be used with mongo 2.0
-apiRouter.delete('/', function(req, res) {
-    console.log("DELETE handler");
-    var objectIDToDelete = req.body.objectID;
-    if (!objectIDToDelete){
-        res.status(500).json({message : 'objectID is null'});
-        return;
-    }
-    var mongoClient = new MongoClient(new Server('localhost', 27017));
-    mongoClient.open(function(err, mongoClient) {
-        if(err){
-            console.log(err);
-            res.status(500).json({message : 'Error connection to the database'});
-        }else{
-            var db = mongoClient.db("sensorData");
-            db.open(function(err, db) {
-              // Fetch a collection to insert document into
-                var collection = db.collection("collection");
-                // Insert a single document
-                collection.deleteOne( { "_id" : objectIDToDelete } ,{}, function(err, result) {
-                    assert.equal(null, err); //make sure error is null
-                    db.close();
-                    res.status(200).json({message : 'Successfully Deleted', dataEntry : result});
-                });
-            });
-        }
-    });
+	var objectToStore = {"sensor":{"type":sensorType,"value":sensorValue},"deviceName": deviceName, "timestamp": timestamp};	
 });
 
 // REGISTER ROUTES
